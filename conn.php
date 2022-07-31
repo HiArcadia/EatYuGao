639b9242f45a7b8385b3e67d4f2e67197fc45e92
渔火Arcadia <85126209+HiArcadia@users.noreply.github.com>
2022-07-31 18:17:41 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,6 +1,6 @@
 <?php
 // Change this to your own configuration
-$link = new mysqli('120.48.13.87:3306','Eat_MiniGame','5YCs2Hsn77F82hkt','eat_minigame');
+$link = new mysqli('120.48.13.87','Eat_MiniGame','5YCs2Hsn77F82hkt','eat_minigame');
 mysqli_set_charset($link, 'utf8');
 if ($link->connect_error) {
     die("Failed to connect: " . $conn->connect_error);
